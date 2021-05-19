9e28d941c046c561fc9f869c30bd4d3305c797fb
filkovskyi <filkovskyi@gmail.com>
2021-05-19 21:08:39 +0300

    master: add search features

--- src/components/ControlPanel.jsx
+++ src/components/ControlPanel.jsx
@@ -1,19 +1,28 @@
 import React, { useState } from 'react';
-import { Radio, Input, Space } from 'antd';
+import {
+  Radio,
+  Space,
+  Row,
+  Col,
+} from 'antd';
 
-const ControlPanel = () => {
-  const [style, setStyle] = useState(1)
+const ControlPanel = ({ mapStyle, setMapStyle, navList }) => {
+  const onChangeHandler = event => setMapStyle(event.target.value)
 
   return (
-    <>
-      <Radio.Group onChange={(value) => setStyle(value)} value={style}>
-        <Space direction="vertical">
-          <Radio value={1}>Option A</Radio>
-          <Radio value={2}>Option B</Radio>
-          <Radio value={3}>Option C</Radio>
-        </Space>
-      </Radio.Group>
-    </>
+    <Space>
+      <Row gutter={24}>
+        <Col xs={24}>
+          <Radio.Group defaultValue={mapStyle} buttonStyle="solid" onChange={onChangeHandler}>
+            <Space direction="horizontal" wrap={true}>
+              {navList.map(item =>
+                <Radio.Button key={item.value} value={item.value}>{item.title}</Radio.Button>)
+              }
+            </Space>
+          </Radio.Group>
+        </Col>
+      </Row>
+    </Space>
   );
 };
 
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,19 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactMapGL from 'react-map-gl';
-const MAPBOX_TOKEN = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
 
-const Map = () => {
-  const [viewport, setViewport] = useState({
-    latitude: 50.4501,
-    longitude: 30.5234,
+
+const Map = ({ mapStyle, defaultLatitude, defaultLongitude }) => {
+  const initialViewportValues = {
+    latitude: defaultLatitude,
+    longitude: defaultLongitude,
     zoom: 11,
     width: '400px',
     height: '400px',
-  });
+    labels: false,
+  }
+  const [viewport, setViewport] = useState(initialViewportValues);
+
+  useEffect(() => {
+    setViewport({
+      ...initialViewportValues,
+      latitude: defaultLatitude,
+      longitude: defaultLongitude,
+    })
+  }, [defaultLatitude, defaultLongitude]);
 
   return (
     <ReactMapGL
       {...viewport}
+      mapStyle={mapStyle}
       mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
       onViewportChange={(viewport) => setViewport(viewport)}
     />
